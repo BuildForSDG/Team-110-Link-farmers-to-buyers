4514f690ff35e11048fc8b7cb27646c7a62cd084
Inyrkz <55382743+Inyrkz@users.noreply.github.com>
2020-05-12 12:45:07 +0100

    lint with flake8

--- src/FarmerModel.py
+++ src/FarmerModel.py
@@ -1,10 +1,10 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy #import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy  # import SQLAlchemy
 import json
 from datetime import datetime
-from settings import app 
+from settings import app
 
 db = SQLAlchemy(app)
+
 
 class User(db.Model):
     '''User class with table user'''
@@ -14,90 +14,126 @@ class User(db.Model):
     phone = db.Column(db.String(11), unique=True, nullable=False)
     password = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(80), unique=True)
-    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow) 
-    role = db.Column(db.String(10), nullable=False, default='Buyer') #setting default role to buyer if not farmer
-    profile = db.relationship('Profile', backref='user_profile', uselist=False, lazy=True)
-    #creating a one to one relationship between the User and Profile
-    #Profile as the first parameter reps the Class Profile to which it's linked to
-    #backref creates a false column in the Profile class with name 'user_profile'
-    #uselist is false for one to one relationship because if you can't have a list, then the
-    # you can only have one value
-    order = db.relationship('Order', backref='user_order', lazy=True) 
-        
+    date_created = db.Column(db.DateTime, nullable=False,
+                             default=datetime.utcnow)
+    role = db.Column(db.String(10), nullable=False, default='Buyer')
+    # setting default role to buyer if not farmer
+    profile_picture = db.Column(db.String(80), default='profile.jpg')
+    location = db.Column(db.String(80))
+    farm_id = db.Column(db.Integer, default='', unique=False, nullable=False)
+    order = db.relationship('Order', backref='user_order', lazy=True)
+
     def json(self):
-        return {'id': self.id, 'full_name': self.full_name, 'Phone_number': self.phone, 
-                'Email': self.email, 'profile_picture':self.profile_picture, 
-                'Farm_name': self.farm_name, 'Farm_Location': self.farm_location}
-        
-    def add_Farmer(_phone, _full_name, _password, _email):
+        return {'id': self.id, 'full_name': self.full_name,
+                'Phone_number': self.phone, 'Email': self.email,
+                'profile_picture': self.profile_picture,
+                'Farm_name': self.farm_name,
+                'Farm_Location': self.farm_location
+                }
+
+    def add_User(_phone, _full_name, _password, _email):
         '''fuction to add farmer to the database'''
-        new_farmer = Farmer(phone=_phone, full_name=_full_name, password=_password, email=_email)
-        db.session.add(new_farmer) #add new farmer to database
-        db.session.commit() # committing changes
+        new_user = User(phone=_phone, full_name=_full_name,
+                        password=_password, email=_email)
+        db.session.add(new_user)  # add new user to database
+        db.session.commit()   # committing changes
 
-    def getAllFarmers():
+    def getAllUsers():
         '''function to return all farmers in the database'''
-        return [Farmer.json(i) for i in Farmer.query.all()]
+        return [User.json(i) for i in User.query.all()]
 
-    def getFarmer(_phone):
-        '''function to get a farmer's profile using phone number as parameter'''
-        #return [Farmer.json(i) for i in Farmer.query.filter_by(phone=_phone).first()]
-        return Farmer.json(Farmer.query.filter_by(phone=_phone).first())
+    def getUser(_phone):
+        '''function to get a user's profile using phone number as parameter'''
+        return User.json(User.query.filter_by(phone=_phone).first())
 
-    def editFarmer(_phone, _full_name, _password, _profile_picture, _farm_name, _farm_location, _email):
-        '''Function to edit a Farmer's profile'''
-        farmer_to_update = Farmer.query.filter_by(phone=_phone).first()
-        farmer_to_update.full_name = _full_name
-        farmer_to_update.phone = _phone
-        farmer_to_update.password = _password
-        farmer_to_update.profile_picture = _profile_picture
-        farmer_to_update.farm_name = _farm_name
-        farmer_to_update.farm_location = _farm_location
-        farmer_to_update.email = _email
-        db.session.commit() #commit changes
+    def editUser(_phone, _full_name, _password, _profile_picture,
+                 _farm_id, _email):
+        '''Function to edit a user's profile'''
+        user_to_update = User.query.filter_by(phone=_phone).first()
+        user_to_update.full_name = _full_name
+        user_to_update.phone = _phone
+        user_to_update.password = _password
+        user_to_update.profile_picture = _profile_picture
+        user_to_update.farm_id = _farm_id
+        user_to_update.email = _email
+        db.session.commit()  # commit changes
 
-    def deleteFarmer(_phone):
-        '''function to delete a farmer's profile'''
-        is_successful = Farmer.query.filter_by(phone=_phone).delete() #filter by phone number and delete
-        db.session.commit() #commiting the new change to our database
-        return bool(is_successful) #checks if deletion is successful, if successful, it will return an integer
-       
+    def deleteUser(_phone):
+        '''function to delete a user's profile'''
+        is_successful = User.query.filter_by(phone=_phone).delete()
+        db.session.commit()  # commiting the new change to our database
+        return bool(is_successful)
+
     def __repr__(self):
-        farmer_object = {
-            'id': self.id, 
-            'full_name': self.full_name, 
-            'Phone_number': self.phone, 
-            'Email': self.email, 
-            'profile_picture':self.profile_picture, 
-            'Farm_name': self.farm_name, 
-            'Farm_Location': self.farm_location
-        }
-        return json.dumps(farmer_object)
+        user_object = {
+            'id': self.id,
+            'full_name': self.full_name,
+            'Phone_number': self.phone,
+            'Email': self.email,
+            'profile_picture': self.profile_picture,
+            'Farm_id': self.farm_id
+    }
+        return json.dumps(user_object)
+
 
 class Farm(db.Model):
-    '''Farm class with table user'''
+    '''Farm class with table farm'''
     __tablename__ = 'farm'
     id = db.Column(db.Integer, primary_key=True)
     farm_name = db.Column(db.String(80))
     farm_location = db.Column(db.String(80))
-    profile_id = db.relationship('Profile', backref='user_farm', lazy=True)
-    # one to many relationship between farm and profile
 
-class Profile(db.Model):
-    '''Profile class with table user'''
-    __tablename__ = 'profile'
-    id = db.Column(db.Integer, primary_key=True)
-    profile_picture = db.Column(db.String(80), default='profile.jpg')
-    location = db.Column(db.String(80), default='')
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), unique=True)
-    farm_id = db.Column(db.Integer, db.ForeignKey('farm.id'))
-    #false columns user_farm and user_profile exists
+    def json(self):
+        return {'id': self.id, 'Farm name': self.farm_name,
+                'Farm Location': self.farm_location}
+
+    def add_Farm(_farm_name, _farm_location):
+        '''fuction to add farm to the database'''
+        new_farm = Farm(farm_name=_farm_name, farm_location=_farm_location)
+        db.session.add(new_farm)   # add new farm to database
+        db.session.commit()   # committing changes
+
+    def getAllFarm():
+        '''function to return all farms in the database'''
+        return [Farm.json(i) for i in Farm.query.all()]
+
+    def getFarm(_id):
+        '''function to get a farm detail using id as parameter'''
+        return Farm.json(Farm.query.filter_by(id=_id).first())
+
+    def editFarm(_id, _farm_name, _farm_location):
+        '''Function to edit a Farm'''
+        farm_to_update = Farm.query.filter_by(id=_id).first()
+        farm_to_update.farm_name = _farm_name
+        farm_to_update.farm_location = _farm_location
+        db.session.commit()  # commit changes
+
+    def deleteFarm(_id):
+        '''function to delete a farmer's profile'''
+        is_successful = Farm.query.filter_by(id=_id).delete()
+        # filter by farm id and delete
+        db.session.commit()   # commiting the new change to our database
+        return bool(is_successful)
+
+    def __repr__(self):
+        farm_object = {
+            'id': self.id,
+            'Farm_name': self.farm_name,
+            'Farm_Location': self.farm_location
+        }
+        return json.dumps(farm_object)
+
 
 class Order(db.Model):
     '''Order class with table order'''
     __tablename__ = 'order'
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), unique=True)
-    order_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow) #no bracket added after the datetime.utcnow so it wont use the current time as at the creation of this app as default time
-    #order_details = db.relationship('OrderDetails', backref='order_item')
-    #false columns user_order exists
+    invoice = db.Column(db.String(20), unique=True, nullable=False)
+    status = db.Column(db.String(20), default='Pending', nullable=False)
+    user_id = db.Column(db.Integer, unique=False, nullable=False)
+    order_date = db.Column(db.DateTime, nullable=False,
+                           default=datetime.utcnow)
+    orders = db.Column(db.Text)
+    # order_details = db.relationship('OrderDetails', backref='order_item')
+    # false columns user_order exists
+
